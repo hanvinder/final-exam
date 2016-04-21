34164cc2e9687351300b6196996ecd5687015af8
hanvinder <hanvinder@gmail.com>
2016-04-21 11:37:47 -0400

    css js html update

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,5 +1,5 @@
-
-//preload the six images first
+/*app.js> Hanvinder Singh Rai>Comp1073-Final Exam> 21/04/2016*/
+// Using images as var
 var face1=new Image()
 face1.src="../img/d1.gif"
 var face2=new Image()
@@ -14,16 +14,20 @@ var face6=new Image()
 face6.src="../img/d6.gif"
 
 function throwdice(){
+	
 //create a random integer between 1 and 6
-var randomdices=Math.round(Math.random()*6);
-document.images["mydice1"].src=eval("face"+randomdices+".src");
-var die1 = document.getElementById("die1");
+	var randomdices=Math.round(Math.random()*6);
 
-die1.innerHTML = randomdices;
+// showing random images on click
+	document.images["mydice1"].src=eval("face"+randomdices+".src");
+
+//  var randomdices used in image and number result for same result
+	var die1 = document.getElementById("die1");
+	die1.innerHTML = randomdices;
 
 //create a random integer between 1 and 6
-var randomdice=Math.round(Math.random()*6);
-document.images["mydice2"].src=eval("face"+randomdice+".src");
-var die2 = document.getElementById("die2");
-die2.innerHTML = randomdice;
+	var randomdice=Math.round(Math.random()*6);
+	document.images["mydice2"].src=eval("face"+randomdice+".src");
+	var die2 = document.getElementById("die2");
+	die2.innerHTML = randomdice;
 }
