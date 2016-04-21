52d5ea12e8ee555bd774dd7ae12071f1d78c8397
hanvinder <hanvinder@gmail.com>
2016-04-21 10:38:00 -0400

    app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -17,17 +17,16 @@ face5.src="../img/d6.gif"
 
 function throwdice(){
 //create a random integer between 0 and 5
-var d1 = Math.floor(Math.random() * 6) + 1;
-document.images["mydice1"].src=eval("face"+d1+".src")
+var randomdice=Math.round(Math.random()*5)
+document.images["mydice1"].src=eval("face"+randomdice+".src")
 var die1 = document.getElementById("die1");
-var d1 = Math.floor(Math.random() * 6) + 1;
-die1.innerHTML = d1;
+die1.innerHTML = randomdice;
 
 //create a random integer between 0 and 5
 var randomdice=Math.round(Math.random()*5)
 document.images["mydice2"].src=eval("face"+randomdice+".src")
 var die2 = document.getElementById("die2");
-var d2 = Math.floor(Math.random() * 6) + 1;
-die2.innerHTML = d2;
+
+die2.innerHTML = randomdice;
 status.innerHTML = "You rolled "+diceTotal+".";
 }
